1fa6ab66107ea8b76b0103be4c33c16766b25736
Tim Lindenau <tim.lindenau@outlook.de>
2024-07-21 13:49:42 +0200

    implement easy deploy also for kaggle_original

--- scripts/kaggle_original.py
+++ scripts/kaggle_original.py
@@ -5,9 +5,13 @@ Note:
 """
 
 import logging
+import os
+from pathlib import Path
 
 import pandas as pd
 from sim import simulate
+
+from lsy_drone_racing.utils.utils import load_config
 
 logger = logging.getLogger(__name__)
 
@@ -15,11 +19,17 @@ logger = logging.getLogger(__name__)
 def main():
     """Run the simulation N times and save the results as 'submission.csv'."""
     n_runs = 10
-    config: str = "config/level3_extra.yaml"
-    controller = "examples/controller.py"
+    config: str = "config/level3.yaml"
     checkpoint="/home/runner/work/lsy_drone_racing_rl/lsy_drone_racing_rl/Documentation/version_more_noise/new_best_solution/baseline_1/rl_model_2200000_steps"
     controller: str = "lsy_drone_racing/controller/rl_controller.py"
-    ep_times = simulate(config=config, controller=controller, checkpoint=checkpoint, n_runs=n_runs, gui=False)
+    
+    checkpoint_base_path_components = checkpoint.split("/")[:-1]
+    checkpoint_base_path = Path(os.path.join(*checkpoint_base_path_components, "config.yaml"))
+    if not checkpoint_base_path.exists():
+        print("Could not restore config from checkpoint. Make sure that the original folder structure is preserved.")
+    rl_config = load_config(checkpoint_base_path).rl_config
+    
+    ep_times = simulate(config=config, controller=controller, checkpoint=checkpoint, n_runs=n_runs, gui=False, override_rl_config=rl_config)
     # Log the number of failed runs if any
 
     if failed := [x for x in ep_times if x is None]:
